7201a7c7c3cf19c3b4061107c5ffa497d3bcefa9
Ubuntu <ubuntu@ip-172-31-25-161.eu-west-2.compute.internal>
2020-08-11 17:30:17 +0000

    fixed init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,18 @@
-sudo docker build -t ursc:0.01 . &&
-sudo docker run --detach --env-file ./config.conf --name ursc --link mongodb:mongodb -p 8083:8083/tcp --rm -ti ursc:0.01 /bin/bash
+usage() { echo "Usage: [-l link container by name source:alias] [-p Expose container on localhost port.]" 1>&2; exit 1; }
+
+while getopts ":l:p:" o; do
+    case "${o}" in
+        l)
+            l="--link ${OPTARG}"
+            ;;
+        p)
+            p="-p ${OPTARG}:8083/tcp"
+            ;;
+        *)
+	    usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+sudo docker build -t ursc:0.01 . && sudo docker run --detach --env-file ./config.conf --name ursc ${l} ${p} --rm -ti ursc:0.01 /bin/bash
